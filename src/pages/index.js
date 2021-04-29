4cf933bda9a67db40d8707c886ebb21ba4cf04f5
jsocias work <jsociaswork@gmail.com>
2021-04-29 11:45:27 +0200

    check visibility title

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,31 +29,36 @@ const BlogIndex = ({ data, location }) => {
       <Bio />
       {posts.map(post => {
         const title = post.frontmatter.title || post.fields.slug
-        return (
-          <article
-            key={post.fields.slug}
-            className="post-list-item"
-            itemScope
-            itemType="http://schema.org/Article"
-          >
-            <header>
-              <h2>
-                <Link to={post.fields.slug} itemProp="url">
-                  <span itemProp="headline">{title}</span>
-                </Link>
-              </h2>
-              <small>{post.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: post.frontmatter.description || post.excerpt,
-                }}
-                itemProp="description"
-              />
-            </section>
-          </article>
-        )
+        let checkVisibilityTitle = "" + title;
+        if (!checkVisibilityTitle.startsWith('--')) {
+          return (
+            <article
+              key={post.fields.slug}
+              className="post-list-item"
+              itemScope
+              itemType="http://schema.org/Article"
+            >
+              <header>
+                <h2>
+                  <Link to={post.fields.slug} itemProp="url">
+                    <span itemProp="headline">{title}</span>
+                  </Link>
+                </h2>
+                <small>{post.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: post.frontmatter.description || post.excerpt,
+                  }}
+                  itemProp="description"
+                />
+              </section>
+            </article>
+          )
+        } else {
+          return "";
+        }
       })}
     </Layout>
   )
